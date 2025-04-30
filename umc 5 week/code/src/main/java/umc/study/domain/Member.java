58429fcd7179c341c9b1ac4cf4c68aa9807ca3f3
Jeong-Ryeol <jwr5336@naver.com>
2025-04-30 16:26:28 +0900

    Add files via upload

---
+++ umc 5 week/code/src/main/java/umc/study/domain/Member.java
@@ -0,0 +1,28 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.enums.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+}
