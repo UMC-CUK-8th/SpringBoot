64877a12995e86d6eb7d47a6d6d344bc9d5f10dc
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2025-05-12 21:23:29 +0900

    Delete umc 6주차 과제 directory

--- umc 6주차 과제/demo/src/main/java/umc/study/repository/MarketRepository/MarketRepository.java
+++
@@ -1,9 +0,0 @@
-package umc.study.repository.MarketRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.study.domain.Market;
-
-public interface  MarketRepository extends JpaRepository<Market, Long>,MarketRepositoryCustom{
-
-}
- 
--- umc 6주차 과제/demo/src/main/java/umc/study/repository/MarketRepository/MarketRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package umc.study.repository.MarketRepository;
-
-import umc.study.domain.Market;
-
-import java.util.List;
-
-public interface MarketRepositoryCustom {
-    List<Market> dynamicQueryWithBooleanBuilder( String name, Float score);
-}
