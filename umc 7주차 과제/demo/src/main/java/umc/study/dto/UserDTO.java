e78f8a67fcaf322c439c8a7ff186c1d9920f0481
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2025-05-13 15:10:57 +0900

    [7주차] 마크/한재민

---
+++ umc 7주차 과제/demo/src/main/java/umc/study/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package umc.study.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String nickname;
+    private String email;
+    private String phoneNumber;
+    private Integer point;
+}
